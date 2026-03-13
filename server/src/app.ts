2b528fa0fd620adfb3cfb7e5f0339ab2e644705c
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-03-13 11:54:13 +0530

    Add express json middleware and error handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,15 +5,25 @@ import type { Request, Response } from 'express';
 import 'dotenv/config';
 import { connectDB } from './config/db';
 
+import authRouter from './routes/v1/auth.route';
+import errorHandler from './handlers/error';
+
 const app = express();
 connectDB();
+
+// Middlewares
+app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
   res.json({
     success: true,
-    message: 'API Running',
-    version: 1.0,
+    message: 'Welcome to API v1.0.0',
+    timestamp: new Date().toISOString(),
   });
 });
+
+app.use('/api/v1/auth', authRouter);
+
+app.use(errorHandler);
 
 export default app;
