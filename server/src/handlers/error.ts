6bd817dbf897d1eefc4164060b8b3d81643d8bea
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-03-13 19:16:06 +0530

    If email already register getting specific error

--- server/src/handlers/error.ts
+++ server/src/handlers/error.ts
@@ -1,11 +1,20 @@
 /** @format */
 
+import { Prisma } from '@prisma/client';
 import { ErrorRequestHandler } from 'express';
+import CustomErrorHandler from './CustomError';
 
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'Internal Server Error';
 
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    // Email already register
+    if (err.code === 'P2002') {
+      const message = 'User already register, Please log in or forgot password';
+      err = new CustomErrorHandler(406, message);
+    }
+  }
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
