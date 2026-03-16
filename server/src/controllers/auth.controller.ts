8006e35f4688f934815701f7b13bb909e26ea786
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-03-16 10:44:30 +0530

    Add refresh token controller and some other changes in res.cookies

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -2,12 +2,20 @@
 
 import asynError from '../handlers/asyncError';
 import { Request } from 'express';
-import { loginDTO, RegisterDTO } from '../schemas/auth.schema';
+import {
+  authHeaderSchema,
+  loginDTO,
+  RegisterDTO,
+} from '../schemas/auth.schema';
 import bcrypt from 'bcryptjs';
 import { prisma } from '../config/db';
-import { creatAccessToken, creatRefreshToken } from '../utils/token';
-import { env } from '../config/env';
+import {
+  creatAccessToken,
+  creatRefreshToken,
+  verifyToken,
+} from '../utils/token';
 import CustomErrorHandler from '../handlers/CustomError';
+import { cookieOptions } from '../utils/cookieOptions';
 
 export const register = asynError(
   async (req: Request<{}, {}, RegisterDTO>, res) => {
@@ -40,17 +48,23 @@ export const register = asynError(
     const accessToken = creatAccessToken(payload);
     const refreshToken = creatRefreshToken(payload);
 
-    res.status(201).cookie('refresh_token', `Bearer ${refreshToken}`, {
-      httpOnly: true,
-      secure: env.NODE_ENV === 'dev' ? false : true,
-      sameSite: env.NODE_ENV === 'dev' ? 'none' : 'strict',
+    await prisma.refreshToken.create({
+      data: {
+        token: refreshToken,
+        userId: id,
+        expireAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      },
     });
-    res.json({
-      success: true,
-      messsage: 'User register successfully',
-      user,
-      accessToken,
-    });
+
+    res
+      .status(201)
+      .cookie('access_token', `Bearer ${accessToken}`, cookieOptions)
+      .cookie('refresh_token', `Bearer ${refreshToken}`, cookieOptions)
+      .json({
+        success: true,
+        messsage: 'User register successfully',
+        user,
+      });
   },
 );
 
@@ -78,15 +92,63 @@ export const login = asynError(async (req: Request<{}, {}, loginDTO>, res) => {
   const accessToken = creatAccessToken(payload);
   const refreshToken = creatRefreshToken(payload);
 
-  res.status(200).cookie('refresh_token', `Bearer ${refreshToken}`, {
-    httpOnly: true,
-    secure: env.NODE_ENV === 'dev' ? false : true,
-    sameSite: env.NODE_ENV === 'dev' ? 'none' : 'strict',
+  await prisma.refreshToken.create({
+    data: {
+      token: refreshToken,
+      userId: id,
+      expireAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    },
   });
-  res.json({
-    success: true,
-    messsage: 'User login successfully',
-    user: { fullname, email: userEmail, role, createdAt },
-    accessToken,
+
+  res
+    .status(200)
+    .cookie('access_token', `Bearer ${accessToken}`, cookieOptions)
+    .cookie('refresh_token', `Bearer ${refreshToken}`, cookieOptions)
+    .json({
+      success: true,
+      messsage: 'User login successfully',
+      user: { fullname, email: userEmail, role, createdAt },
+      accessToken,
+    });
+});
+
+export const refreshToken = asynError(async (req, res) => {
+  if (!req.headers['refresh_token'])
+    throw new CustomErrorHandler(401, 'Unauthorized');
+
+  const result = authHeaderSchema.safeParse(req.headers['refresh_token']);
+  if (!result.success)
+    throw new CustomErrorHandler(401, 'Invalid authorization header');
+
+  let refreshToken = result.data!;
+  const payload = await prisma.refreshToken.findUnique({
+    where: { token: refreshToken },
   });
+
+  if (!payload) throw new CustomErrorHandler(401, 'Invalid token');
+
+  const user = await prisma.user.findUnique({ where: { id: payload.userId } });
+  if (!user) throw new CustomErrorHandler(401, 'Unauthorized');
+
+  const { id, email, role } = user;
+  const newAccessToken = creatAccessToken({ id, userEmail: email, role });
+  const newRefreshToken = creatRefreshToken({ id, userEmail: email, role });
+
+  await prisma.refreshToken.delete({ where: { id: payload.id } });
+  await prisma.refreshToken.create({
+    data: {
+      token: newRefreshToken,
+      userId: id,
+      expireAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    },
+  });
+
+  res
+    .status(200)
+    .cookie('access_token', `Bearer ${newAccessToken}`, cookieOptions)
+    .cookie('refresh_token', `Bearer ${newRefreshToken}`, cookieOptions)
+    .json({
+      success: true,
+      messsage: 'Token refreshed successfully',
+    });
 });
