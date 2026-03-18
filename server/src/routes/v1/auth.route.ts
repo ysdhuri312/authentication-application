bfb43f727c8c4b23001ad6350f394b5df779d043
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-03-18 11:08:53 +0530

    Create logout router

--- server/src/routes/v1/auth.route.ts
+++ server/src/routes/v1/auth.route.ts
@@ -3,16 +3,19 @@
 import { Router } from 'express';
 import {
   login,
+  logout,
   refreshToken,
   register,
 } from '../../controllers/auth.controller';
 import validate from '../../middlewares/validate';
 import { loginSchema, registerSchema } from '../../schemas/auth.schema';
+import { isAuthorized } from '../../middlewares/auth';
 
 const router = Router();
 
 router.post('/register', validate(registerSchema), register);
 router.post('/login', validate(loginSchema), login);
 router.post('/refresh', refreshToken);
+router.post('/logout', isAuthorized, logout);
 
 export default router;
