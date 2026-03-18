7559e36087191e1ac6ad8a03c34a6c300c387027
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-03-18 20:04:25 +0530

    Create header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+const Header = () => {
+  return (
+    <section className='w-full fixed border-b border-[rgba(0,0,0,0.1)] backdrop-blur-sm'>
+      <div className='container'>
+        <div className='h-15 flex justify-between items-center'>
+          <p className='font-medium text-xl'>LOGO.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Header;
