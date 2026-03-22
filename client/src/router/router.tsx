6e0e57996086ebccb913075de056bc247319f75a
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-03-22 20:04:09 +0530

    Router setup using react-router

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -6,6 +6,7 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Layout from '../components/Layout';
+import About from '../pages/About';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: 'register',
         element: <Register />,
+      },
+      {
+        path: 'about',
+        element: <About />,
       },
     ],
   },
